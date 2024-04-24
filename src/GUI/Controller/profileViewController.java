5a4e637e8a6b499142d41be4a7becf6e6be38884
Rune Krogh <runekrogh20@gmail.com>
2024-04-24 13:25:01 +0200

    Flere UI ting
    Profile Creator/Editor samt Team Creator/Editor er lavet

--- src/GUI/Controller/profileViewController.java
+++ src/GUI/Controller/profileViewController.java
@@ -1,4 +1,40 @@
 package GUI.Controller;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 public class profileViewController {
+
+
+
+
+
+    @FXML
+    private void openProfileEditor() {
+        openNewWindow("../View/profileEditor.fxml");
+    }
+
+    @FXML
+    private void openProfileCreator() {
+        openNewWindow("../View/profileCreator.fxml");
+    }
+
+    @FXML
+    private void openNewWindow(String fxmlPath) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            stage.setResizable(false);
+            stage.setTitle("Schneider Electric");
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
