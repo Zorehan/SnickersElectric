5645b5d6f19cbd7af507a18abdeed3f97402dca6
Rune Krogh <runekrogh20@gmail.com>
2024-05-20 12:07:10 +0200

    Icons færdig + Dashboard start view

--- src/GUI/Controller/View/mainViewController.java
+++ src/GUI/Controller/View/mainViewController.java
@@ -4,17 +4,30 @@ import DAL.DatabaseConnector;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import util.Exception;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class mainViewController {
+public class mainViewController implements Initializable{
 
+    @FXML private Button btnDashboard;
+    @FXML private Button btnProfiles;
+    @FXML private Button btnTeams;
+    @FXML private Button btnScenarios;
+    @FXML private Button btnLogs;
+    @FXML private Button btnStatistics;
     @FXML private BorderPane borderPane;
 
     @FXML private void handleProfilesButtonClick() {
@@ -56,5 +69,26 @@ public class mainViewController {
                         + "See error log for technical details."
         );
         alert.showAndWait();
+    }
+
+
+    private void setButtonGraphic(Button button, String imagePath) {
+            Image image = new Image(imagePath);
+            ImageView imageView = new ImageView(image);
+            imageView.setFitHeight(32);  // Set desired height
+            imageView.setFitWidth(32);   // Set desired width
+            button.setGraphic(imageView);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        loadAndViewFXML("../../View/dashboardView.fxml");
+        setButtonGraphic(btnDashboard, "icons/dashboard.png");
+        setButtonGraphic(btnProfiles, "icons/person.png");
+        setButtonGraphic(btnTeams, "icons/teams.png");
+        setButtonGraphic(btnScenarios, "icons/scenario.png");
+        setButtonGraphic(btnLogs, "icons/logs.png");
+        setButtonGraphic(btnStatistics, "icons/statistics.png");
     }
 }
