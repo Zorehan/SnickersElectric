c7b85fd63a501eea6bfddb5a8f8ef998fa92dc95
Nicklas Kramer <kramernicklas@gmail.com>
2024-05-27 22:14:10 +0200

    Modular Time Range

--- src/GUI/Controller/View/dashboardViewController.java
+++ src/GUI/Controller/View/dashboardViewController.java
@@ -9,13 +9,16 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.*;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.ListView;
 
 import java.net.URL;
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class dashboardViewController implements Initializable {
     private ProfileModel profileModel = ProfileModel.getInstance();
@@ -29,10 +32,16 @@ public class dashboardViewController implements Initializable {
     private ComboBox<String> comboSortType;
     @FXML
     private ListView<Object> listAvailableItems;
+    @FXML
+    private DatePicker dateFrom, dateTo;
     private List<String> dateCategories;
+    private List<Profile> selectedProfiles;
+    private DateTimeFormatter formatter;
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         dateCategories = new ArrayList<>();
+        selectedProfiles = new ArrayList<>();
+        formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
         listAvailableItems.getItems().addAll(profileModel.getObservableProfiles());
 
@@ -43,7 +52,7 @@ public class dashboardViewController implements Initializable {
 
     @FXML
     private void clickComboSortType(ActionEvent actionEvent) {
-        //initData((Profile) listAvailableItems.getSelectionModel().getSelectedItem());
+        refreshGraph();
     }
 
     private Number getSortType(HistoricProfile profile, String type) {
@@ -61,7 +70,6 @@ public class dashboardViewController implements Initializable {
 
     @FXML
     private void clickAdd(ActionEvent actionEvent) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         XYChart.Series<String, Number> data = new XYChart.Series<>();
         Profile profile = (Profile) listAvailableItems.getSelectionModel().getSelectedItem();
         List<Profile> historicProfiles = profileModel.getHistoricProfile(profile);
@@ -76,23 +84,20 @@ public class dashboardViewController implements Initializable {
             HistoricProfile hProfile = (HistoricProfile) p;
             String formattedDate = hProfile.getDate().format(formatter);
             XYChart.Data<String, Number> newData = new XYChart.Data<>(formattedDate, getSortType(hProfile, comboSortType.getSelectionModel().getSelectedItem()));
-            if(!chart.getData().contains(newData)) {
-                data.getData().add(newData);
-                data.setName(profile.getName());
-            }
+            newData.setExtraValue(hProfile);
+            data.getData().add(newData);
+            data.setName(profile.getName());
+
             if(!dateCategories.contains(formattedDate)) {
                 dateCategories.add(formattedDate);
             }
         }
         if(!data.getData().isEmpty()){
-            dateCategories.sort(String::compareTo);
+            sortDates(dateCategories);
             dateAxis.setCategories(FXCollections.observableArrayList(dateCategories));
             chart.getData().add(data);
+            selectedProfiles.add(profile);
         }
-    }
-
-    @FXML
-    private void clickRemove(ActionEvent actionEvent) {
     }
 
     @FXML
@@ -111,5 +116,94 @@ public class dashboardViewController implements Initializable {
     private void clearGraph() {
         dateCategories.clear();
         chart.getData().clear();
+        selectedProfiles.clear();
+    }
+
+    private void refreshGraph() {
+        List<Profile> profiles = new ArrayList<>(selectedProfiles);
+        clearGraph();
+
+        for(Profile profile : profiles) {
+            XYChart.Series<String, Number> data = new XYChart.Series<>();
+            List<Profile> historicProfiles = profileModel.getHistoricProfile(profile);
+
+            historicProfiles.sort((profile1, profile2) -> {
+                HistoricProfile historicProfile1 = (HistoricProfile) profile1;
+                HistoricProfile historicProfile2 = (HistoricProfile) profile2;
+                return historicProfile1.getDate().compareTo(historicProfile2.getDate());
+            });
+
+            for (Profile p : historicProfiles) {
+                HistoricProfile hProfile = (HistoricProfile) p;
+                String formattedDate = hProfile.getDate().format(formatter);
+                XYChart.Data<String, Number> newData = new XYChart.Data<>(formattedDate, getSortType(hProfile, comboSortType.getSelectionModel().getSelectedItem()));
+                newData.setExtraValue(hProfile);
+                data.getData().add(newData);
+                data.setName(profile.getName());
+
+                if (!dateCategories.contains(formattedDate)) {
+                    dateCategories.add(formattedDate);
+                }
+            }
+            if (!data.getData().isEmpty()) {
+                sortDates(dateCategories);
+                dateAxis.setCategories(FXCollections.observableArrayList(dateCategories));
+                chart.getData().add(data);
+                selectedProfiles.add(profile);
+            }
+        }
+    }
+
+    @FXML
+    private void pickedDateFrom(ActionEvent actionEvent) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate selectedDate = dateFrom.getValue();
+        String formattedDate = formatter.format(selectedDate);
+        System.out.println(formattedDate);
+
+        List<String> dates = new ArrayList<>(dateAxis.getCategories());
+        dates.sort(String::compareTo);
+
+        List<String> datesToRemove = new ArrayList<>();
+        for (String date : dates) {
+            LocalDate parsedDate = LocalDate.parse(date, formatter);
+            if (parsedDate.isBefore(selectedDate)) {
+                datesToRemove.add(date);
+            }
+        }
+        dateAxis.getCategories().removeAll(datesToRemove);
+    }
+
+    @FXML
+    private void pickedDateTo(ActionEvent actionEvent) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate selectedDate = dateTo.getValue();
+        String formattedDate = formatter.format(selectedDate);
+        System.out.println(formattedDate);
+
+        List<String> dates = new ArrayList<>(dateAxis.getCategories());
+        dates.sort(String::compareTo);
+
+        List<String> datesToRemove = new ArrayList<>();
+        for (String date : dates) {
+            LocalDate parsedDate = LocalDate.parse(date, formatter);
+            if (parsedDate.isAfter(selectedDate)) {
+                datesToRemove.add(date);
+            }
+        }
+        dateAxis.getCategories().removeAll(datesToRemove);
+    }
+
+    public  List<String> sortDates(List<String> dateList) {
+        // Convert date strings to LocalDate objects
+        List<LocalDate> localDates = dateList.stream()
+                .map(date -> LocalDate.parse(date, formatter))
+                .sorted()
+                .toList();
+
+        // Convert LocalDate objects back to strings
+        return localDates.stream()
+                .map(date -> date.format(formatter))
+                .collect(Collectors.toList());
     }
 }
